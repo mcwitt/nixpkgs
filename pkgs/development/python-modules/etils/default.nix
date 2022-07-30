d64cd5c9e98aae9bd8d72ec456aed67b680f0209
Matt Wittmann <mcwitt@gmail.com>
2022-07-29 18:19:44 -0700

    Condense formatting

--- pkgs/development/python-modules/etils/default.nix
+++ pkgs/development/python-modules/etils/default.nix
@@ -45,80 +45,19 @@ buildPythonPackage rec {
   ];
 
   passthru.optional-dependencies = rec {
-    array-types = [
-    ]
-    ++ enp;
-
-    ecolab = [
-      jupyter
-      numpy
-      mediapy
-    ]
-    ++ enp
-    ++ epy;
-
-    edc = [
-    ]
-    ++ epy;
-
-    enp = [
-      numpy
-    ]
-    ++ epy;
-
-    epath = [
-      importlib-resources
-      typing-extensions
-      zipp
-    ]
-    ++ epy;
-
-    epy = [
-      typing-extensions
-    ];
-
-    etqdm = [
-      absl-py
-      tqdm
-    ]
-    ++ epy;
-
-    etree = [
-    ]
-    ++ array-types
-    ++ epy
-    ++ enp
-    ++ etqdm;
-
-    etree-dm = [
-      dm-tree
-    ]
-    ++ etree;
-
-    etree-jax = [
-      jax
-    ]
-    ++ etree;
-
-    etree-tf = [
-      tensorflow
-      etree
-    ]
-    ++ etree;
-
-    all = [
-    ]
-    ++ array-types
-    ++ ecolab
-    ++ edc
-    ++ enp
-    ++ epath
-    ++ epy
-    ++ etqdm
-    ++ etree
-    ++ etree-dm
-    ++ etree-jax
-    ++ etree-tf;
+    array-types = enp;
+    ecolab = [ jupyter numpy mediapy ] ++ enp ++ epy;
+    edc = epy;
+    enp = [ numpy ] ++ epy;
+    epath = [ importlib-resources typing-extensions zipp ] ++ epy;
+    epy = [ typing-extensions ];
+    etqdm = [ absl-py tqdm ] ++ epy;
+    etree = array-types ++ epy ++ enp ++ etqdm;
+    etree-dm = [ dm-tree ] ++ etree;
+    etree-jax = [ jax ] ++ etree;
+    etree-tf = [ tensorflow etree ] ++ etree;
+    all = array-types ++ ecolab ++ edc ++ enp ++ epath ++ epy ++ etqdm
+      ++ etree ++ etree-dm ++ etree-jax ++ etree-tf;
   };
 
   pythonImportsCheck = [
