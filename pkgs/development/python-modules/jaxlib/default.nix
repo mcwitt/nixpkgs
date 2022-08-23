906e512094e3ce38db505b919736ef80a970b673
Matt Wittmann <mcwitt@gmail.com>
2022-08-22 21:53:32 -0700

    Also patch files in jaxlib/cuda

--- pkgs/development/python-modules/jaxlib/default.nix
+++ pkgs/development/python-modules/jaxlib/default.nix
@@ -244,7 +244,7 @@ let
       #    loading multiple extensions in the same python program due to duplicate protobuf DBs.
       # 3) Patch python path in the compiler driver.
       preBuild = ''
-        for src in ./jaxlib/*.{cc,h}; do
+        for src in ./jaxlib/*.{cc,h} ./jaxlib/cuda/*.{cc,h}; do
           sed -i 's@include/pybind11@pybind11@g' $src
         done
       '' + lib.optionalString cudaSupport ''
